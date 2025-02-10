af630f3c51827c7b0cf222017980d1fa2d935e16
dashath0097 <dashathk0440@gmail.com>
2025-02-10 19:22:22 +0530

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -21,11 +21,14 @@ variable "admin_username" {
 
 variable "admin_password" {
   default = "SecureP@ssw0rd!"  # Not recommended for production
-
 }
 
 variable "nic_name" {
   default = "spacelift-nic"  # Replace with existing NIC name
+}
+
+variable "public_ip_name" {
+  default = "spacelift-public-ip"  # Replace with your existing Public IP name
 }
 
 # Reference an existing Network Interface
@@ -34,14 +37,20 @@ data "azurerm_network_interface" "nic" {
   resource_group_name = var.resource_group_name
 }
 
+# Fetch the associated Public IP
+data "azurerm_public_ip" "public_ip" {
+  name                = var.public_ip_name
+  resource_group_name = var.resource_group_name
+}
+
 # Create a Virtual Machine
 resource "azurerm_linux_virtual_machine" "vm" {
-  name                = var.vm_name
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  size                = "Standard_B1s"
-  admin_username      = var.admin_username
-  admin_password      = var.admin_password
+  name                  = var.vm_name
+  resource_group_name   = var.resource_group_name
+  location              = var.location
+  size                  = "Standard_B1s"
+  admin_username        = var.admin_username
+  admin_password        = var.admin_password
   disable_password_authentication = false
 
   network_interface_ids = [data.azurerm_network_interface.nic.id]
@@ -64,6 +73,7 @@ output "vm_id" {
   value = azurerm_linux_virtual_machine.vm.id
 }
 
+# Corrected Output for Public IP
 output "vm_public_ip" {
-  value = data.azurerm_network_interface.nic.ip_configuration[0].public_ip_address
+  value = data.azurerm_public_ip.public_ip.ip_address
 }
