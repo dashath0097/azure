0c17c2d903bb074946d0827f1e96c928d7209c2c
dashath0097 <dashathk0440@gmail.com>
2025-02-10 20:06:20 +0530

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -1,77 +1,30 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
-
-  cloud {
-    organization = "your-spacelift-org"
-
-    workspaces {
-      name = "azure-blob-storage"
-    }
-  }
-}
-
 provider "azurerm" {
   features {}
-
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
 }
 
-# Create Resource Group
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
+  name     = "spacelift-rg"
+  location = "East US"
 }
 
-# Create Storage Account (Equivalent to S3 in Azure)
-resource "azurerm_storage_account" "storage" {
-  name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  allow_blob_public_access = false
+resource "azurerm_virtual_network" "vnet" {
+  name                = "spacelift-vnet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
 }
 
-# Create Blob Storage Container (Equivalent to S3 Bucket)
-resource "azurerm_storage_container" "container" {
-  name                  = "spacelift-container"
-  storage_account_name  = azurerm_storage_account.storage.name
-  container_access_type = "private"
+resource "azurerm_subnet" "subnet" {
+  name                 = "spacelift-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
-# Outputs
-output "storage_account_name" {
-  value = azurerm_storage_account.storage.name
+output "vnet_id" {
+  value = azurerm_virtual_network.vnet.id
 }
 
-output "storage_account_primary_key" {
-  value     = azurerm_storage_account.storage.primary_access_key
-  sensitive = true
-}
-
-output "storage_container_url" {
-  value = azurerm_storage_container.container.id
-}
-
-# Variables
-variable "subscription_id" {}
-variable "client_id" {}
-variable "client_secret" {}
-variable "tenant_id" {}
-variable "resource_group_name" {
-  default = "spacelift-rg"
-}
-variable "location" {
-  default = "East US"
-}
-variable "storage_account_name" {
-  default = "spaceliftstorage1234" # Must be globally unique
+output "subnet_id" {
+  value = azurerm_subnet.subnet.id
 }
