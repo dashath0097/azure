19be4dcc5ae2de7e4cee6929e9077056345cb467
dashath0097 <dashathk0440@gmail.com>
2025-02-12 11:45:14 +0530

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -45,6 +45,36 @@ resource "azurerm_public_ip" "public_ip" {
   allocation_method   = "Static"
 }
 
+resource "azurerm_network_security_group" "nsg" {
+  name                = "spacelift-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "AllowSSHInbound"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "0.0.0.0/0"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowSSHOutbound"
+    priority                   = 101
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "0.0.0.0/0"
+  }
+}
+
 resource "azurerm_network_interface" "nic" {
   name                = "spacelift-nic"
   location            = azurerm_resource_group.rg.location
@@ -56,6 +86,11 @@ resource "azurerm_network_interface" "nic" {
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
+}
+
+resource "azurerm_network_interface_security_group_association" "nsg_association" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
